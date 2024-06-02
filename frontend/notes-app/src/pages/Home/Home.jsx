536d8e9782c0a1305c54ae608fbc6b8b8113d1dc
Manuel <manuelxgabriel@gmail.com>
2024-06-02 00:29:37 -0700

    Added the visuals for the dashboard

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import Navbar from "../../components/Navbar/Navbar.jsx";
+import NoteCard from "../../components/Cards/NoteCard.jsx";
+import {MdAdd} from "react-icons/md";
 
 const Home = () => {
   return (
-    <>
-      <Navbar/>
-    </>
-  )
-}
+        <>
+          <Navbar/>
+            <div className="container mx-auto">
+                <div className="grid grid-cols-3 gap-4 mt-8">
+                    <NoteCard
+                        title="Meeting on Tuesday 7th April"
+                        date="3rd Apr 2024"
+                        content="Bring all of the important files"
+                        tags="#meeting"
+                        isPinned={true}
+                        onEdit={()=>{}}
+                        onDelete={()=>{}}
+                        onPinNote={()=>{}}
+                    />
+
+                </div>
+            </div>
+
+            <button className="
+                w-16 h-16
+                flex items-center justify-center
+                rounded-2xl bg-primary hover:bg-blue-600
+                absolute right-10 button-10"
+                    onClick={()=> {}}>
+                <MdAdd className="text-[32px] text-white"/>
+            </button>
+
+        </>
+  );
+};
 
 export default Home
